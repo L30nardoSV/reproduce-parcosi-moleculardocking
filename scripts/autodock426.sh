b584f631580dd3d4838d42e9f1250643544350f9
Leonardo Solis V <leonardosolis86@gmail.com>
2020-10-08 16:24:23 +0200

    Uploading script for running serial baseline AutoDock

---
+++ scripts/autodock426.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Running original single-threaded Autodock4.2.6
+
+./autodock4 -p dpf_autodock426/2048000/1u4d_rand-0.dpf -l results_autodock426/1u4d_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1xoz_rand-0.dpf -l results_autodock426/1xoz_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1yv3_rand-0.dpf -l results_autodock426/1yv3_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1owe_rand-0.dpf -l results_autodock426/1owe_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1oyt_rand-0.dpf -l results_autodock426/1oyt_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1ywr_rand-0.dpf -l results_autodock426/1ywr_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1t46_rand-0.dpf -l results_autodock426/1t46_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/2bm2_rand-0.dpf -l results_autodock426/2bm2_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1mzc_rand-0.dpf -l results_autodock426/1mzc_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1r55_rand-0.dpf -l results_autodock426/1r55_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/5wlo_rand-0.dpf -l results_autodock426/5wlo_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1kzk_rand-0.dpf -l results_autodock426/1kzk_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/3s8o_rand-0.dpf -l results_autodock426/3s8o_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/5kao_rand-0.dpf -l results_autodock426/5kao_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1hfs_rand-0.dpf -l results_autodock426/1hfs_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/1jyq_rand-0.dpf -l results_autodock426/1jyq_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/2d1o_rand-0.dpf -l results_autodock426/2d1o_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/3drf_rand-0.dpf -l results_autodock426/3drf_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/4er4_rand-0.dpf -l results_autodock426/4er4_singlecore_autodock426.dlg
+./autodock4 -p dpf_autodock426/2048000/3er5_rand-0.dpf -l results_autodock426/3er5_singlecore_autodock426.dlg
