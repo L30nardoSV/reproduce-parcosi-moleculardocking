16bbd31c45c894866baf7d5199de0ed384bc2d77
Leonardo Solis V <leonardosolis86@gmail.com>
2021-01-21 23:58:42 +0100

    Removing gunzip decompression. Not needed anymore as data is stored decompressed via lfs.

--- prepare_inputs.sh
+++ prepare_inputs.sh
@@ -9,14 +9,3 @@ set -o xtrace
 # Pulling dataset
 # Source: https://github.com/L30nardoSV/AD-GPU_miniset_20
 git submodule update --init --recursive
-
-# Preparing dataset as some subfiles were compressed
-EVAL_INPUTS_DIR=./AD-GPU_miniset_20/data
-
-for dir in ${EVAL_INPUTS_DIR}/*; do
-    if [ -d "${dir}" ]; then
-        echo "${dir}"
-        cd ${dir} && gunzip protein.*.map.gz
-        cd ../../..
-    fi
-done
