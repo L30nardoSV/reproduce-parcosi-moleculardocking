8681ad1e29f355bf3f12cb44a21be8e416cdebfd
Leonardo Solis <solis@esa.informatik.tu-darmstadt.de>
2021-02-11 11:40:02 +0100

    Adding support for cuda version in script nwi.

--- evaluate_numwi.sh
+++ evaluate_numwi.sh
@@ -3,7 +3,8 @@
 source parcosi_dataset.sh
 source numwi.sh
 
-ADGPU_OPENCL_BINS=(./autodock_gpu_32wi ./autodock_gpu_64wi ./autodock_gpu_128wi ./autodock_gpu_256wi)
+ADGPU_OPENCL_BINS=(./autodock_gpu_32wi_ocl ./autodock_gpu_64wi_ocl ./autodock_gpu_128wi_ocl ./autodock_gpu_256wi_ocl)
+ADGPU_CUDA_BINS=(./autodock_gpu_32wi_cuda ./autodock_gpu_64wi_cuda ./autodock_gpu_128wi_cuda ./autodock_gpu_256wi_cuda)
 
 # Bash functions
 function select_device() {
@@ -12,11 +13,20 @@ function select_device() {
   echo " "
   echo "${info}: GPU? "
   read -p "[Y]: " TEST_GPU
+  echo "${info}: CUDA? "
+  read -p "[Y]: " TEST_CUDA
+
   if [ "${TEST_GPU}" == "Y" ]; then
     echo " "
     echo "${info}: Type a meaningful label for your GPU device."
     read -p "E.g.: [v100] [a100] [mi50] [mi100] [vega64] [etc]: " LABEL_GPU
-    RES_GPU_DIR=results_numwi_${LABEL_GPU}
+
+    if [ "${TEST_CUDA}" == "Y" ]; then
+      RES_GPU_DIR=results_numwi_cuda_${LABEL_GPU}
+    else
+      RES_GPU_DIR=results_numwi_opencl_${LABEL_GPU}
+    fi
+
     if [ ! -d ${RES_GPU_DIR} ]; then
       mkdir ${RES_GPU_DIR}
     else
@@ -27,7 +37,11 @@ function select_device() {
   echo " "
   echo "${info}: Device(s) to be tested: "
   if [ "${TEST_GPU}" == "Y" ]; then
-    echo "\"${LABEL_GPU}\" GPU "
+    if [ "${TEST_CUDA}" == "Y" ]; then
+      echo "\"${LABEL_GPU}\" GPU (CUDA)"
+    else
+      echo "\"${LABEL_GPU}\" GPU (OpenCL)"
+    fi
   fi
   if [ "${TEST_GPU}" != "Y" ]; then
     echo "${info}: No device chosen."
@@ -41,15 +55,27 @@ function verify_binaries_exist_in_local_folder() {
   echo "${info}: Verifying that AutoDock-GPU binaries are present in current folder."
   echo " "
   if [ "${TEST_GPU}" == "Y" ]; then
-    for i_adgpu_bin in ${ADGPU_OPENCL_BINS[@]}; do
-        if [ -f "${i_adgpu_bin}" ]; then
-        echo "${info}: \"${i_adgpu_bin}\" exists."
-        else
-        echo "${info}: \"${i_adgpu_bin}\" does not exist. Make sure binary is copied over first!"
-        echo "${info}: Terminated."
-        exit 9999 # Die with error code 9999
-        fi
-    done
+    if [ "${TEST_CUDA}" == "Y" ]; then
+      for i_adgpu_bin in ${ADGPU_CUDA_BINS[@]}; do
+          if [ -f "${i_adgpu_bin}" ]; then
+          echo "${info}: \"${i_adgpu_bin}\" exists."
+          else
+          echo "${info}: \"${i_adgpu_bin}\" does not exist. Make sure CUDA binary is copied over first!"
+          echo "${info}: Terminated."
+          exit 9999 # Die with error code 9999
+          fi
+      done
+    else
+      for i_adgpu_bin in ${ADGPU_OPENCL_BINS[@]}; do
+          if [ -f "${i_adgpu_bin}" ]; then
+          echo "${info}: \"${i_adgpu_bin}\" exists."
+          else
+          echo "${info}: \"${i_adgpu_bin}\" does not exist. Make sure OpenCL binary is copied over first!"
+          echo "${info}: Terminated."
+          exit 9999 # Die with error code 9999
+          fi
+      done
+    fi
   fi
 }
 
@@ -59,7 +85,13 @@ verify_binaries_exist_in_local_folder
 
 # numwi test
 if [ "${TEST_GPU}" == "Y" ]; then
+  if [ "${TEST_CUDA}" == "Y" ]; then
+    for i_adgpu_bin in ${ADGPU_CUDA_BINS[@]}; do
+        numwi ${i_adgpu_bin} ${RES_GPU_DIR}
+    done
+  else
     for i_adgpu_bin in ${ADGPU_OPENCL_BINS[@]}; do
         numwi ${i_adgpu_bin} ${RES_GPU_DIR}
     done
+  fi
 fi
