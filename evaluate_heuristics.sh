83e9b180fa2cfefea9f6520795354d2ac2053b24
Leonardo Solis V <leonardosolis86@gmail.com>
2021-02-21 22:12:34 +0100

    Fix script for heuristics.

--- evaluate_heuristics.sh
+++ evaluate_heuristics.sh
@@ -14,11 +14,11 @@ verify_binaries_exist_in_local_folder
 # heuristics test
 if [ "${TEST_GPU}" == "Y" ]; then
   if [ "${TEST_CUDA}" == "Y" ]; then
-    for i_adgpu_bin in ${ADGPU_OPENCL_BINS[@]}; do
+    for i_adgpu_bin in ${ADGPU_CUDA_BINS[@]}; do
       heuristics ${i_adgpu_bin} ${RES_GPU_DIR} ${DEVNUM}
     done
   else
-    for i_adgpu_bin in ${ADGPU_CUDA_BINS[@]}; do
+    for i_adgpu_bin in ${ADGPU_OPENCL_BINS[@]}; do
       heuristics ${i_adgpu_bin} ${RES_GPU_DIR} ${DEVNUM}
     done
   fi
