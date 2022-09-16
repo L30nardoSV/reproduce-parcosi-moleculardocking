8f51db21c1f0b30e4f440f53c29064c5194e318e
Leonardo Solis <solis@esa.informatik.tu-darmstadt.de>
2022-09-16 11:21:24 +0200

    Minor comment update.

--- parcosi_dataset.sh
+++ parcosi_dataset.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace
 
-# Listing inputs in PARCOSI dataset
+# Listing dataset of 20 inputs
 # Source: https://gitlab.com/L30nardoSV/ad-gpu_miniset_20.git
 PARCOSI_DATASET=(1u4d 1xoz 1yv3 1owe 1oyt 1ywr 1t46 2bm2 1mzc 1r55 5wlo 1kzk 3s8o 5kao 1hfs 1jyq 2d1o 3drf 4er4 3er5)
 
