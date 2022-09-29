5a71a5d4e73259398069f2ebf422cd00053f691f
Leonardo <leonardosolis86@gmail.com>
2022-09-29 13:43:52 +0200

    Parsing CUDA/OpenCL/DPC++ Setup time.

--- parse_adgpu_log.py
+++ parse_adgpu_log.py
@@ -15,10 +15,15 @@ def retrieve_runtimes(filename):
 	"""Retrieve runtimes (s) from log file"""
 
 	# Metacharacter "+" should be escaped
-	label_start_measurement = "\+ ./autodock_g"
+	label_start_measurement = "\+ ./autodock_"
 	searchpattern_start_measurement = "^" + label_start_measurement
 
-	# Metacharacter "^" forces searching for a line starting with search pattern
+	# Metacharacter "." matches any character except newline character
+	label_time_setup = "Setup time"
+	searchpattern_time_setup = "." + label_time_setup
+	index_time_setup = 3
+
+	# Metacharacter "^" searches for a line starting with a given pattern
 	label_time_restofsetup = "Rest of Setup time"
 	searchpattern_time_restofsetup = "^" + label_time_restofsetup
 	index_time_restofsetup = 4
@@ -43,6 +48,7 @@ def retrieve_runtimes(filename):
 
 		for line in lines:
 			found_start_measurement = re.search(searchpattern_start_measurement, line)
+			found_time_setup = re.search(searchpattern_time_setup, line)
 			found_time_restofsetup = re.search(searchpattern_time_restofsetup, line)
 			found_time_docking = re.search(searchpattern_time_docking, line)
 			found_time_shutdown = re.search(searchpattern_time_shutdown, line)
@@ -52,6 +58,12 @@ def retrieve_runtimes(filename):
 				found_new_measurement = True
 				count_new_measurement = count_new_measurement + 1
 				print("\ncount_new_measurement: ", count_new_measurement)
+
+			if found_time_setup:
+				split_line = re.split("\s", line)
+				if split_line[0] != "Rest":	# Avoids matching pattern "found_time_restofsetup"
+					time_setup = split_line[index_time_setup]
+					print(split_line[0] + " " + label_time_setup, ":\t", time_setup) # Prints CUDA/OpenCL/DPC++ label at the beginning
 
 			if found_time_restofsetup:
 				split_line = re.split("\s", line)
