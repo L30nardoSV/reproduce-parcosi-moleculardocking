09efb0bdb2f2d6db9708e750bb37273dc3d7fa1c
Leonardo <leonardosolis86@gmail.com>
2022-09-29 15:30:29 +0200

    Storing measurements into lists.

--- parse_adgpu_log.py
+++ parse_adgpu_log.py
@@ -53,9 +53,17 @@ def retrieve_runtimes(filename):
 	searchpattern_time_processing = "^" + label_time_processing
 	index_time_processing = 2
 
+	#
+	num_of_entries_per_dlg = 40
+	num_measurements_per_entry = 12
+	num_elements_per_entry = num_measurements_per_entry
+	list_measurements = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'] for i in range(num_of_entries_per_dlg)]
+
+
+	print(list_measurements)
+
 	with open(filename, "rt") as myfile:	# open file for reading text
 		lines = myfile.readlines()
-
 		found_new_measurement = False
 		count_new_measurement = 0
 
@@ -71,7 +79,6 @@ def retrieve_runtimes(filename):
 			if found_start_measurement:
 				print("\n")
 				found_new_measurement = True
-				count_new_measurement = count_new_measurement + 1
 				print("count_new_measurement: ", count_new_measurement)
 
 				split_line = re.split("\s", line) # Splits AD-GPU command based on the blank space character
@@ -82,6 +89,11 @@ def retrieve_runtimes(filename):
 				version = name_dlg[index_dlg_version]
 				pdb = name_dlg[index_dlg_pdb]
 				ls = name_dlg[index_dlg_ls]
+				list_measurements[count_new_measurement][0] = device
+				list_measurements[count_new_measurement][1] = numwi
+				list_measurements[count_new_measurement][2] = version
+				list_measurements[count_new_measurement][3] = pdb
+				list_measurements[count_new_measurement][4] = ls
 				print("Device:\t", device)
 				print("NUMWI:\t", numwi)
 				print("Version:\t", version)
@@ -93,37 +105,49 @@ def retrieve_runtimes(filename):
 				if split_line[0] != "Rest":	# Avoids matching pattern "found_time_restofsetup"
 					time_setup = split_line[index_time_setup]
 					time_setup = re.sub("s$", " ", time_setup) # Replaces trailing "s" character with blank space
+					list_measurements[count_new_measurement][5] = time_setup
 					print(split_line[0] + " " + label_time_setup, ":\t", time_setup) # Prints CUDA/OpenCL/DPC++ label at the beginning
 
 			if found_time_restofsetup:
 				split_line = re.split("\s", line)
 				time_restofsetup = split_line[index_time_restofsetup]
 				time_restofsetup = re.sub("s$", " ", time_restofsetup) # Replaces trailing "s" character with blank space
+				list_measurements[count_new_measurement][6] = time_restofsetup
 				print(label_time_restofsetup, ":\t", time_restofsetup)
 
 			if found_time_docking:
 				split_line = re.split("\s", line)
 				time_docking = split_line[index_time_docking]
 				time_docking = re.sub("s$", " ", time_docking) # Replaces trailing "s" character with blank space
+				list_measurements[count_new_measurement][7] = time_docking
 				print(label_time_docking, ":\t", time_docking)
 
 			if found_time_shutdown:
 				split_line = re.split("\s", line)
 				time_shutdown = split_line[index_time_shutdown]
 				time_shutdown = re.sub("s$", " ", time_shutdown) # Replaces trailing "s" character with blank space
+				list_measurements[count_new_measurement][8] = time_shutdown
 				print(label_time_shutdown, ":\t", time_shutdown)
 
 			if found_time_job:
 				split_line = re.split("\s", line)
 				time_job = split_line[index_time_job]
 				time_job_wait = split_line[index_time_job_wait]
+				list_measurements[count_new_measurement][9] = time_job
+				list_measurements[count_new_measurement][10] = time_job_wait
 				print("Job time", ":\t", time_job)
 				print("Job wait time", ":\t", time_job_wait)
 
 			if found_time_processing:
 				split_line = re.split("\s", line)
 				time_processing = split_line[index_time_processing]
+				list_measurements[count_new_measurement][11] = time_processing
 				print(label_time_processing, ":\t", time_processing)
+
+				if found_new_measurement == True:
+					print(list_measurements[count_new_measurement])
+					found_new_measurement = False
+					count_new_measurement = count_new_measurement + 1
 
 def main():
 	# First argument is the log file
