a74427041e914fd358ea92495ca63aa056c56c89
Leonardo <leonardosolis86@gmail.com>
2022-09-29 11:55:30 +0200

    Parsing times: rest-of-setup, docking, shutdown, processing.

--- parse_adgpu_log.py
+++ parse_adgpu_log.py
@@ -18,31 +18,48 @@ def retrieve_runtimes(filename):
 	searchpattern_time_restofsetup = "^" + label_time_restofsetup
 	index_time_restofsetup = 4
 
+	label_time_docking = "Docking time"
+	searchpattern_time_docking = "^" + label_time_docking
+	index_time_docking = 2
+
+	label_time_shutdown = "Shutdown time"
+	searchpattern_time_shutdown = "^" + label_time_shutdown
+	index_time_shutdown = 2
+
+	label_time_processing = "Processing time"
+	searchpattern_time_processing = "^" + label_time_processing
+	index_time_processing = 2
+
 	with open(filename, "rt") as myfile:	# open file for reading text
 		lines = myfile.readlines()
 #		found_new_measurement = False
 #		count_new_measurement = 0
 
 		for line in lines:
-#			found_time_restofsetup = re.search("^Rest of Setup time", line)
 			found_time_restofsetup = re.search(searchpattern_time_restofsetup, line)
-			found_time_docking = re.search("^Docking time", line)
-			found_time_shutdown = re.search("^Shutdown time", line)
-			found_time_processing = re.search("^Processing time", line)
+			found_time_docking = re.search(searchpattern_time_docking, line)
+			found_time_shutdown = re.search(searchpattern_time_shutdown, line)
+			found_time_processing = re.search(searchpattern_time_processing, line)
 
 			if found_time_restofsetup:
 				split_line = re.split("\s", line)
 				time_restofsetup = split_line[index_time_restofsetup]
-				print(label_time_restofsetup, ": ", time_restofsetup)
+				print(label_time_restofsetup, ":\t", time_restofsetup)
 
 			if found_time_docking:
-				print("found_time_docking")
+				split_line = re.split("\s", line)
+				time_docking = split_line[index_time_docking]
+				print(label_time_docking, ":\t", time_docking)
 
 			if found_time_shutdown:
-				print("found_time_shutdown")
+				split_line = re.split("\s", line)
+				time_shutdown = split_line[index_time_shutdown]
+				print(label_time_shutdown, ":\t", time_shutdown)
 
 			if found_time_processing:
-				print("found_time_processing")
+				split_line = re.split("\s", line)
+				time_processing = split_line[index_time_processing]
+				print(label_time_processing, ":\t", time_processing)
 
 #			if line.startswith('+ ./autodock_gpu_'):
 #				found_new_measurement = True
