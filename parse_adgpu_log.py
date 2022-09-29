85d67d9bd04565cb01540630b164c16f17e45791
Leonardo <leonardosolis86@gmail.com>
2022-09-29 15:37:04 +0200

    Controlling amount of print-outs with extra function argument.

--- parse_adgpu_log.py
+++ parse_adgpu_log.py
@@ -11,7 +11,7 @@ def parse_filename(filename):
 	name_field_list = name_field_list.split('_')
 	print(name_field_list)
 
-def retrieve_runtimes(filename):
+def retrieve_runtimes(filename, is_print_enabled):
 	"""Retrieve runtimes (s) from log file"""
 
 	# Metacharacter "+" is escaped with preceding "\"
@@ -92,11 +92,12 @@ def retrieve_runtimes(filename):
 				list_measurements[count_new_measurement][2] = version
 				list_measurements[count_new_measurement][3] = pdb
 				list_measurements[count_new_measurement][4] = ls
-				print("Device:\t", device)
-				print("NUMWI:\t", numwi)
-				print("Version:\t", version)
-				print("Test case:\t", pdb)
-				print("Local search:\t", ls)
+				if (is_print_enabled == True):
+					print("Device:\t", device)
+					print("NUMWI:\t", numwi)
+					print("Version:\t", version)
+					print("Test case:\t", pdb)
+					print("Local search:\t", ls)
 
 			if found_time_setup:
 				split_line = re.split("\s", line)
@@ -104,28 +105,32 @@ def retrieve_runtimes(filename):
 					time_setup = split_line[index_time_setup]
 					time_setup = re.sub("s$", " ", time_setup) # Replaces trailing "s" character with blank space
 					list_measurements[count_new_measurement][5] = time_setup
-					print(split_line[0] + " " + label_time_setup, ":\t", time_setup) # Prints CUDA/OpenCL/DPC++ label at the beginning
+					if (is_print_enabled == True):
+						print(split_line[0] + " " + label_time_setup, ":\t", time_setup) # Prints CUDA/OpenCL/DPC++ label at the beginning
 
 			if found_time_restofsetup:
 				split_line = re.split("\s", line)
 				time_restofsetup = split_line[index_time_restofsetup]
 				time_restofsetup = re.sub("s$", " ", time_restofsetup) # Replaces trailing "s" character with blank space
 				list_measurements[count_new_measurement][6] = time_restofsetup
-				print(label_time_restofsetup, ":\t", time_restofsetup)
+				if (is_print_enabled == True):
+					print(label_time_restofsetup, ":\t", time_restofsetup)
 
 			if found_time_docking:
 				split_line = re.split("\s", line)
 				time_docking = split_line[index_time_docking]
 				time_docking = re.sub("s$", " ", time_docking) # Replaces trailing "s" character with blank space
 				list_measurements[count_new_measurement][7] = time_docking
-				print(label_time_docking, ":\t", time_docking)
+				if (is_print_enabled == True):
+					print(label_time_docking, ":\t", time_docking)
 
 			if found_time_shutdown:
 				split_line = re.split("\s", line)
 				time_shutdown = split_line[index_time_shutdown]
 				time_shutdown = re.sub("s$", " ", time_shutdown) # Replaces trailing "s" character with blank space
 				list_measurements[count_new_measurement][8] = time_shutdown
-				print(label_time_shutdown, ":\t", time_shutdown)
+				if (is_print_enabled == True):
+					print(label_time_shutdown, ":\t", time_shutdown)
 
 			if found_time_job:
 				split_line = re.split("\s", line)
@@ -133,14 +138,16 @@ def retrieve_runtimes(filename):
 				time_job_wait = split_line[index_time_job_wait]
 				list_measurements[count_new_measurement][9] = time_job
 				list_measurements[count_new_measurement][10] = time_job_wait
-				print("Job time", ":\t", time_job)
-				print("Job wait time", ":\t", time_job_wait)
+				if (is_print_enabled == True):
+					print("Job time", ":\t", time_job)
+					print("Job wait time", ":\t", time_job_wait)
 
 			if found_time_processing:
 				split_line = re.split("\s", line)
 				time_processing = split_line[index_time_processing]
 				list_measurements[count_new_measurement][11] = time_processing
-				print(label_time_processing, ":\t", time_processing)
+				if (is_print_enabled == True):
+					print(label_time_processing, ":\t", time_processing)
 
 				if found_new_measurement == True:
 					print(list_measurements[count_new_measurement])
@@ -151,7 +158,7 @@ def main():
 	# First argument is the log file
 	log_file = sys.argv[1]
 	parse_filename(log_file)
-	retrieve_runtimes(log_file)
+	retrieve_runtimes(log_file, False) # True
 
 main()
 
