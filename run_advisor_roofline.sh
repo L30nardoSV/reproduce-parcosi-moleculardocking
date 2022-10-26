8af569bf3e2bc8adeed577786d8c4c26cd8fc7b8
Leonardo <leonardosolis86@gmail.com>
2022-10-26 17:06:01 +0200

    Separating launch of Advisor commands.

--- run_advisor_roofline.sh
+++ run_advisor_roofline.sh
@@ -89,10 +89,6 @@ print_cmd () {
 run_cmd () {
 	print_cmd "$1"
 	$1
-	if [ "${RUN_ALSO_ADADELTA}" == "y" ]; then
-		print_cmd "$2"
-		$2
-	fi
 }
 
 run_gpu_roofline() {
@@ -112,10 +108,17 @@ run_gpu_roofline() {
 	cmd_roofline_ad_1="${cmd_roofline_shorcut} --project-dir=${output_folder_ad} -- ${adgpu_cmd_ad}"
 	cmd_roofline_ad_2="${cmd_perfmodeling} --project-dir=${output_folder_ad}"
 
-	local cmd_local_sw="${cmd_roofline_sw_1} && ${cmd_roofline_sw_2}"
-	local cmd_local_ad="${cmd_roofline_ad_1} && ${cmd_roofline_ad_2}"
+#	local cmd_local_sw="${cmd_roofline_sw_1} && ${cmd_roofline_sw_2}"
+#	local cmd_local_ad="${cmd_roofline_ad_1} && ${cmd_roofline_ad_2}"
+#	run_cmd "${cmd_local_sw}" "${cmd_local_ad}"
 
-	run_cmd "${cmd_local_sw}" "${cmd_local_ad}"
+	run_cmd "${cmd_roofline_sw_1}"
+	run_cmd "${cmd_roofline_sw_2}"
+
+	if [ "${RUN_ALSO_ADADELTA}" == "y" ]; then
+		run_cmd "${cmd_roofline_ad_1}"
+		run_cmd "${cmd_roofline_ad_2}"
+	fi
 }
 
 
